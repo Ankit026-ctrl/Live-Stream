2ceaf4c976af163b8ef5fd784d1eb4beec41dd33
iamankitsingh267 <78903552+Ankit026-ctrl@users.noreply.github.com>
2024-05-22 10:47:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 jwplayer("my-video").setup({
-	file: "https://prod-sports-hin-gm.jiocinema.com/hls/live/2100329/hd_akamai_androidmob_avc_hin_ipl_s2_m2200524/master.m3u8",
+	file: "https://prod-sports-hin-gm.jiocinema.com/hls/live/2100329/hd_akamai_androidmob_avc_hin_ipl_s2_m2220524/master.m3u8",
 	image: "https://y20india.in/rr-vs-rcb-ipl-match-19-preview/",
 	primary: "html5",
 	advertising: {
