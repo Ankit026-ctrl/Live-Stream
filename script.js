3e5282b8063fb346e0566346f04dc25207b3d9ba
iamankitsingh267 <78903552+Ankit026-ctrl@users.noreply.github.com>
2024-06-29 19:11:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 jwplayer("my-video").setup({
-	file: "https://pubads.g.doubleclick.net/ssai/event/hQXQ8apwTJeGKlGD7Oi7lQ/master.m3u8",
+	file: "https://vkvsd75.mycdn.me/hls/6652439694055.m3u8/sig/wLrakuJt0yw/srcIp/117.96.11.227/urls/45.136.20.59/expires/1720117982090/clientType/14/srcAg/CHROME_ANDROID/fromCache/1/mid/7597513649127/id/6652439694055/6652439694055/6652439694055_high/index.m3u8?p",
 	image: "https://cricket.one/cricket-analysis/ocs-ipl-match-prediction-today-ipl-2024-rr-vs-rcb-who-will-win-todays-eliminator/664d66b8fdc09c66a5326584",
 	primary: "html5",
 	advertising: {
